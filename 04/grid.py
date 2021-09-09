514e73488581e6160c2b0e5c17249ee4d9f9273b
hyeSUon <hyeonsu@kpu.ac.kr>
2021-09-09 23:39:11 +0900

    DRILL 04 완성
    2020180030 이현수

---
+++ 04/grid.py
@@ -0,0 +1,20 @@
+import turtle
+cnt = 12;
+turtle.penup()
+turtle.goto(-100,-100)
+turtle.pendown()
+while(cnt > 0):
+    if(cnt == 6):
+        turtle.right(90)
+        turtle.penup()
+        turtle.forward(100)
+        turtle.pendown()
+    turtle.forward(500)
+    turtle.penup()
+    turtle.left(180)
+    turtle.forward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.pendown()
+    cnt = cnt-1
